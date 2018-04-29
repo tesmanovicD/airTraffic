203933a1bcf917f50d355a520c26addabd7ab4e1
tesmanovicD <danites007@gmail.com>
2018-04-30 01:34:08 +0200

    opening single air flight details, adding styling and validating html

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   getLocation()
-    .then(position => { return getAirplanesList(position) })
-    .then(userPosition => setInterval(function(){
-        getAirplanesList(userPosition)
+    .then(position => {
+      return getAirplanesList(position)
+    })
+    .then(userPosition => setInterval(function() {
+      getAirplanesList(userPosition)
     }, 60000))
     .catch(function(err) {
+      $('.loadingCircle').hide();
       $(".airplanes-list tbody").html(
         `<tr>
           <td colspan="3" class="alert alert-danger">
@@ -13,9 +16,16 @@ $(document).ready(function() {
         </tr>`)
     })
 
+  $("#close-dialog").click(function() {
+    $(".flight-details").hide();
+    $(".nearby-airplanes").removeClass("col-md-5");
+    $(".nearby-airplanes").addClass("col-md-12");
+  })
+
 })
 
 const API_URL = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json";
+let airFlights;
 
 function getLocation() {
   return new Promise(function(resolve, reject) {
@@ -26,23 +36,31 @@ function getLocation() {
 function getAirplanesList(position) {
   const userLatitude = position.coords.latitude;
   const userLongitude = position.coords.longitude;
-  const between = {start: 0, end: 100}
+  const between = {
+    start: 0,
+    end: 100
+  }
   $.ajax({
     url: `${API_URL}?lat=${userLatitude}&lng=${userLongitude}&fDstL=${between.start}&fDstU=${between.end}`,
     dataType: "jsonp",
     beforeSend: function() {
       $('.airplanes-list tbody .airplane-data').hide();
-      $('.airplanes-list tbody .airplane-data').html("");
+      $('.airplanes-list tbody').html("");
       $('.loadingCircle').show();
     },
     success: function(data) {
-      let airFlights = data.acList.slice();
+      airFlights = data.acList.slice();
       airFlights.sort((a, b) => b["Alt"] - a["Alt"])
       airFlights.forEach(flight => {
         let planeSide = flight.long < 0 ? "west" : "east";
         $(".airplanes-list tbody").append(
-          `<tr class="airplane-data">
-            <td><img src="img/airplane_${planeSide}.png" class="img-responsive" title="Going to ${planeSide} side"/></td>
+          `<tr class="airplane-data" onClick="getFlightDetails(${flight.Id})">
+            <td>
+            <img src="img/airplane_${planeSide}.png"
+                 class="img-responsive"
+                 title="Going to ${planeSide} side"
+                 alt="airplane-icon"/>
+            </td>
             <td>${flight.Alt} ft</td>
             <td>${flight.CNum}</td>
           </tr>`);
@@ -52,4 +70,22 @@ function getAirplanesList(position) {
     }
   })
   return position;
+}
+
+function getFlightDetails(id) {
+  let flight = airFlights.find(x => x.Id == id);
+  let companyName = flight.Op;
+  companyName ? companyName : "Unknown";
+  companyName = companyName.replace(/[^\w\s]/gi, '');
+  $("#airplane-manufacturer").html(flight.Man);
+  $("#airplane-model").html(flight.Mdl);
+  $("#airplane-company").html(companyName)
+  companyName = companyName.toLowerCase().replace(/\s/g, "_");
+  $("#airplane-logo").attr({
+    src: `img/company/${companyName}.png`,
+    alt: companyName
+  });
+  $(".nearby-airplanes").removeClass("col-md-12");
+  $(".nearby-airplanes").addClass("col-md-5");
+  $(".flight-details").show();
 }
